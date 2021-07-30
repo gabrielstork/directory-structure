2912922dd80ca213d026a0913faec0eb025f1bca
storkdeveloper@gmail.com <storkdeveloper@gmail.com>
2021-07-30 10:42:40 -0300

    class Tree

--- directory_tree.py
+++ directory_tree.py
@@ -2,11 +2,26 @@ import os
 import emoji
 
 
-def directory_tree(path):
-    content = [c for c in os.listdir(path)]
+class Tree:
+    def __init__(self, directory):
+        self.directory = directory
+        self.base = os.path.basename(self.directory)
+        self.folder = ''
+        self.file = ''
 
-    for c in content:
-        if os.path.isdir(os.path.join(path, c)):
-            print(emoji.emojize(f':file_folder: {c}'))
-        else:
-            print(emoji.emojize(f':page_facing_up: {c}'))
+    def __str__(self):
+        self.base = emoji.emojize(f':open_file_folder: {self.base}\n')
+
+        folders = [folder for folder in os.listdir(self.directory)
+                   if os.path.isdir(os.path.join(self.directory, folder))]
+
+        files = [file for file in os.listdir(self.directory)
+                 if os.path.isfile(os.path.join(self.directory, file))]
+
+        for folder in folders:
+            self.folder += emoji.emojize(f'|—:file_folder: {folder}\n')
+
+        for file in files:
+            self.file += emoji.emojize(f'|—:page_facing_up: {file}\n')
+
+        return self.base + self.folder + self.file
