44136ca02eba642c774b2b99511fd526ac4130ab
storkdeveloper@gmail.com <storkdeveloper@gmail.com>
2021-08-03 10:53:15 -0300

    new methods

--- directory_tree.py
+++ directory_tree.py
@@ -2,26 +2,57 @@ import os
 import emoji
 
 
+EMOJI_A = emoji.emojize(':open_file_folder:')
+EMOJI_B = emoji.emojize(':file_folder:')
+EMOJI_C = emoji.emojize(':page_facing_up:')
+
+
 class Tree:
-    def __init__(self, directory):
+    def __init__(self, directory, complete=False):
         self.directory = directory
-        self.base = os.path.basename(self.directory)
+        self.complete = complete
+
+        self.base = f'{EMOJI_A} {os.path.basename(self.directory)}'
         self.folder = ''
         self.file = ''
 
-    def __str__(self):
-        self.base = emoji.emojize(f':open_file_folder: {self.base}\n')
+    def get_complete_base(self):
+        directories = self.directory.split('/')
+        self.base = f'{EMOJI_A} {directories[0]}'
 
+        for directory in directories[1:]:
+            lines = self.base.count('\n')
+            
+            self.base += f'\n{"  " * (lines)}|_{EMOJI_A} {directory}'
+        
+        return lines + 1
+
+    def get_folders(self):
         folders = [folder for folder in os.listdir(self.directory)
                    if os.path.isdir(os.path.join(self.directory, folder))]
 
+        return folders
+    
+    def get_files(self):
         files = [file for file in os.listdir(self.directory)
                  if os.path.isfile(os.path.join(self.directory, file))]
 
+        return files
+
+    def __str__(self):
+        if self.complete:
+            lines = self.get_complete_base()
+        else:
+            lines = 0
+
+        folders = self.get_folders()
+
         for folder in folders:
-            self.folder += emoji.emojize(f'|—:file_folder: {folder}\n')
+            self.folder += f'\n{"  "*(lines)}|_{EMOJI_B} {folder}'
+
+        files = self.get_files()
 
         for file in files:
-            self.file += emoji.emojize(f'|—:page_facing_up: {file}\n')
+            self.file += f'\n{"  "*(lines)}|_{EMOJI_C} {file}'
 
         return self.base + self.folder + self.file
