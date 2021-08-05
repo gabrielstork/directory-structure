d2506de9dd0ef26181f33049fd83e3a63e835d68
storkdeveloper@gmail.com <storkdeveloper@gmail.com>
2021-08-05 10:25:48 -0300

    lines is now an attribute

--- directory_tree.py
+++ directory_tree.py
@@ -14,7 +14,7 @@ class Tree:
 
         self.folder = ''
         self.file = ''
- 
+
     def get_base(self):
         if self.complete:
             drive = self.path.split('/')[0]
@@ -23,40 +23,34 @@ class Tree:
             directories = self.path.split('/')[1:]
 
             for directory in directories:
-                lines = self.base.count('\n')
+                self.lines = self.base.count('\n')
 
-                self.base += f'\n{"  " * (lines)}|_{EMOJI_A} {directory}'
+                self.base += f'\n{"  " * (self.lines)}|_{EMOJI_A} {directory}'
 
-            return lines + 1
+            self.lines += 1
         else:
             directory = self.path.split('/')[-1]
             self.base = f'{EMOJI_A} {directory}'
 
-            return 0
+            self.lines = 0
 
     def get_folders(self):
         folders = [folder for folder in os.listdir(self.path)
                    if os.path.isdir(os.path.join(self.path, folder))]
 
-        return folders
-    
+        for folder in folders:
+            self.folder += f'\n{"  "*(self.lines)}|_{EMOJI_B} {folder}'
+
     def get_files(self):
         files = [file for file in os.listdir(self.path)
                  if os.path.isfile(os.path.join(self.path, file))]
 
-        return files
+        for file in files:
+            self.file += f'\n{"  "*(self.lines)}|_{EMOJI_C} {file}'
 
     def __str__(self):
-        lines = self.get_base()
-
-        folders = self.get_folders()
-
-        for folder in folders:
-            self.folder += f'\n{"  "*(lines)}|_{EMOJI_B} {folder}'
-
-        files = self.get_files()
-
-        for file in files:
-            self.file += f'\n{"  "*(lines)}|_{EMOJI_C} {file}'
+        self.get_base()
+        self.get_folders()
+        self.get_files()
 
         return self.base + self.folder + self.file
