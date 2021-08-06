e1efaf5a8b6b8f605a4a027926b5b5f8a792ac7f
storkdeveloper@gmail.com <storkdeveloper@gmail.com>
2021-08-06 09:35:45 -0300

    file renamed

---
+++ tree.py
@@ -0,0 +1,54 @@
+import os
+import emoji
+
+
+EMOJI_A = emoji.emojize(':open_file_folder:')
+EMOJI_B = emoji.emojize(':file_folder:')
+EMOJI_C = emoji.emojize(':page_facing_up:')
+
+
+class Tree:
+    def __init__(self, path: str, complete: bool = False) -> None:
+        self.path = path
+        self.complete = complete
+
+        self.folder = ''
+        self.file = ''
+
+    def get_base(self) -> None:
+        if self.complete:
+            drive = self.path.split('/')[0]
+            directories = self.path.split('/')[1:]
+            self.base = f'{EMOJI_A} {drive}'
+
+            for directory in directories:
+                self.lines = self.base.count('\n')
+                self.base += f'\n{"  " * (self.lines)}|_{EMOJI_A} {directory}'
+
+            self.lines += 1
+        else:
+            directory = self.path.split('/')[-1]
+            self.base = f'{EMOJI_A} {directory}'
+
+            self.lines = 0
+
+    def get_folders(self) -> None:
+        folders = [folder for folder in os.listdir(self.path)
+                   if os.path.isdir(os.path.join(self.path, folder))]
+
+        for folder in folders:
+            self.folder += f'\n{"  "*(self.lines)}|_{EMOJI_B} {folder}'
+
+    def get_files(self) -> None:
+        files = [file for file in os.listdir(self.path)
+                 if os.path.isfile(os.path.join(self.path, file))]
+
+        for file in files:
+            self.file += f'\n{"  "*(self.lines)}|_{EMOJI_C} {file}'
+
+    def __str__(self) -> str:
+        self.get_base()
+        self.get_folders()
+        self.get_files()
+
+        return self.base + self.folder + self.file
