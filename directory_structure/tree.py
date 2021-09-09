aec790c774c0248902365d71fe0298881966a1ec
Gabriel <storkdeveloper@gmail.com>
2021-09-09 11:51:35 -0300

    new private method, minor improvements

--- directory_structure/tree.py
+++ directory_structure/tree.py
@@ -9,7 +9,7 @@ EMOJI_FILE = emoji.emojize(':page_facing_up:')
 
 class Tree:
     """
-    A class to print directory tree structures.
+    A class to generate directory tree structures.
 
     Parameters
     ----------
@@ -24,12 +24,13 @@ class Tree:
 
         self._lines = 0
         self._base = []
-        self._folder = []
-        self._file = []
+        self._folders = []
+        self._files = []
+        self._tree = self._make_tree()
 
     def _space(self) -> str:
         """
-        Returns a string containing spaces according to the folder or file
+        Return a string containing spaces according to the folder or file
         location.
 
         Returns:
@@ -38,13 +39,12 @@ class Tree:
             String containing spaces.
         """
         spaces = f'\n{"  " * self._lines}'
-
         return spaces
 
     def _get_base(self) -> None:
         """
-        Depending on the absolute attribute, get all or only the last folder of 
-        the path.
+        Depending on the absolute attribute, get all or only the last folder
+        of the path.
         """
         self.path = self.path.resolve()
 
@@ -69,7 +69,7 @@ class Tree:
         """
         for folder in self.path.iterdir():
             if folder.is_dir():
-                self._folder.append(
+                self._folders.append(
                     f'{self._space()}|_{EMOJI_FOLDER} {folder.name}'
                 )
 
@@ -79,18 +79,31 @@ class Tree:
         """
         for file in self.path.iterdir():
             if file.is_file():
-                self._file.append(
+                self._files.append(
                     f'{self._space()}|_{EMOJI_FILE} {file.name}'
                 )
 
-    def __str__(self) -> str:
+    def _make_tree(self) -> str:
+        """
+        Call other private methods and then sum its joined results to make the
+        directory tree structure.
+
+        Returns:
+        --------
+        tree : str
+            Complete directory tree structure.
+        """
         self._get_base()
         self._base = ''.join(self._base)
 
         self._get_folders()
-        self._folder = ''.join(self._folder)
+        self._folders = ''.join(self._folders)
 
         self._get_files()
-        self._file = ''.join(self._file)
+        self._files = ''.join(self._files)
 
-        return self._base + self._folder + self._file
+        tree = self._base + self._folders + self._files
+        return tree
+
+    def __str__(self) -> str:
+        return self._tree
